6724119aa4ca329477b48e791add1ce7fce70c14
Dean Nemphos <deannemphos@gmail.com>
2024-12-04 13:35:42 -0500

    Initial Commit

---
+++ LittleRouteTests/LittleRouteTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleRouteTests.swift
+//  LittleRouteTests
+//
+//  Created by Dean Nemphos on 12/4/24.
+//
+
+import Testing
+@testable import LittleRoute
+
+struct LittleRouteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
