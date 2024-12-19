7fbcc15cc42e0081d28792cd7edcf08dc9dcfb4b
Dean Nemphos <deannemphos@gmail.com>
2024-12-19 03:45:47 -0500

    Added basic Song data structure. Added folders for organization

---
+++ LittleRoute/Data Handling/Song.swift
@@ -0,0 +1,25 @@
+//
+//  Song.swift
+//  LittleRoute
+//
+//  Created by Dean Nemphos on 12/18/24.
+//
+
+import SwiftData
+
+@Model
+final class Song {
+    var title: String
+    var location: String                // context in which the song will play
+    var populationMin: Int = 0          // minimum population of an area where the song will play
+    var populationMax: Int = 1000000000 // maximum population of an area where the song will play -- default overly large
+    
+    init(title: String, location: String, populationMin: Int?, populationMax: Int?) {
+        self.title = title
+        self.location = location
+        self.populationMin = populationMin ?? <#default value#>
+        self.populationMax = populationMax!
+    }
+}
+
+
