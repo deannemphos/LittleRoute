0a31369d530e4df1479307233bc4e8b0d3fe6e8c
Dean Nemphos <deannemphos@gmail.com>
2025-04-05 03:16:07 -0400

    added params for file and artist names, changed locations to an array so one song can fit multiple contexts

--- LittleRoute/Data Handling/Song.swift
+++ LittleRoute/Data Handling/Song.swift
@@ -6,17 +6,27 @@
 //
 
 import SwiftData
+import AVFoundation
 
 @Model
 final class Song {
     var title: String
-    var location: String                // context in which the song will play
+    var songName: String /* {
+        // This is the actual file name of the song, without the extension
+        return title.replacingOccurrences(of: ".mp3", with: "")
+    } */
+    var artist: String? = nil // optional artist name
+    // @TODO: set possible locations to an enum
+    var locations: [String]                // context in which the song will play
+    // @TODO: set population min/max to be dependent on location automatically
     var populationMin: Int = 0          // minimum population of an area where the song will play
     var populationMax: Int = 1000000000 // maximum population of an area where the song will play -- default overly large
     
-    init(title: String, location: String, populationMin: Int?, populationMax: Int?) {
+    init(title: String, songName: String, artist: String?, locations: [String], populationMin: Int?, populationMax: Int?) {
         self.title = title
-        self.location = location
+        self.songName = songName.replacingOccurrences(of: ".mp3", with: "")
+        self.artist = artist
+        self.locations = locations
         self.populationMin = populationMin ?? self.populationMin // if no value is provided, default to 0
         self.populationMax = populationMax ?? self.populationMax // ""
     }
