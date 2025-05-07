0dc0f257ae89a86cf244d687df83b0872ae6103a
Dean Nemphos <deannemphos@gmail.com>
2025-05-07 16:40:22 -0400

    Created outline for separate manager for the AVAudioPlayer and added reference to ContentView

--- LittleRoute/Data Handling/AudioPlayerManager.swift
+++ LittleRoute/Data Handling/AudioPlayerManager.swift
@@ -7,3 +7,12 @@
 
 import Foundation
 import AVFoundation
+
+class AudioPlayerManager: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    @Published var isPlaying: Bool = false
+    @Published var currentSong: Song?
+    
+    private var audioPlayer: AVAudioPlayer?
+    
+    static let shared = AudioPlayerManager()
+}
