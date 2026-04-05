f8601f25a94f1726721f13c8b25b1fdbb53c5887
deannemphos <deannemphos@gmail.com>
2026-04-05 19:06:54 -0400

    Added Context struct for bundling POIs

--- LittleRoute/Data Handling/Context.swift
+++ LittleRoute/Data Handling/Context.swift
@@ -1,7 +1,14 @@
 import AVFoundation
 import SwiftData
+import MKPointOfInterestCategory
 
-@Model
-final class Context {
-    
+// Users can use this to create their own custom context pools if the predefined groupings
+// aren't good enough for them
+//
+// @NOTE: define Context with var when instantiating, otherwise categories become immutable
+struct Context {
+    var categories: MKPointOfInterestCategory[] // all POI categories in this context
+    var isEndabled: Bool                        // are we checking for this context
+    let name: String                            // user defined name of context
+    let contextColor: Color                     // color/theme for background when in context 
 }
