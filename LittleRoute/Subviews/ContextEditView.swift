2eeff9c2f4ba5039e311027e8aff4bef62a8156e
deannemphos <deannemphos@gmail.com>
2026-04-05 19:54:54 -0400

    outline ContextEditView

---
+++ LittleRoute/Subviews/ContextEditView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContextEditView: View {
+    // @TODO: fill this with the following fields:
+    // name
+    // contexts[]
+    // color (multiple)
+    //
+    // We will populate these with the Context (struct) passed through)
+}
+
+struct ContextEditView_Previews: PreviewProvider {
+    static var previews: some View {
+            context: "PLACEHOLDER, EDIT LATER"
+        }
+}
